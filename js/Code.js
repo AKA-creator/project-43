7d810a041df93209efcdd6e90d97645ccc57b007
AKA-creator <74938084+AKA-creator@users.noreply.github.com>
2021-05-27 19:42:42 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIABLE"
+const accessCode2 = "FUNCTION"
+const accessCode3 = "DATABASE"
+
+function clues(){
+    fill("white")
+    textSize(15)
+    text("REVBAILA", 100, 50)
+    fill("lightblue")
+    text("Always changing, not constant !!", 100, 70)
+    fill("white")
+    textSize(15)
+    text("TFUNIONC", 700, 150)
+    fill("lightblue")
+    text("performs a specific task", 700, 170)
+    fill("white")
+    textSize(15)
+    text("BATDASEA", 100, 250)
+    fill("lightblue")
+    text("Allows you to store data", 100, 270)
+}
